4c04dcfa4f9b9b8089ccf1ed260e8ee5efcfd8d9
Tina Majchrzak <majchrzakt@lanecc.edu>
2025-11-04 22:02:13 -0800

    Sleep Please

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Sleep Now!");
-  console.log("clicked");
+  res.send("Sleep Please!");
+  console.log("refreshed");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
